1ad85454103bcaf142e1c96ea3b3c5e4a6218be7
FabricioCespedes <fabriciocespedes16@gmail.com>
2021-11-17 11:15:41 -0600

    Add project files.

---
+++ FabricioCespedesProyectoFase2/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FabricioCespedesProyectoFase2
+{
+    public static class Config
+    {
+        public static string getCadenaConexion
+        {
+            get { return Properties.Settings.Default.CadenaConexion; }
+        }
+
+    }
+}
